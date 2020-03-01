0db76ddd6757ddf09251f0013176949580456290
Eric Bragas <eric@flywheelsoftware.com>
2020-03-01 23:08:42 +0000

    add posts list

--- app/routes.py
+++ app/routes.py
@@ -4,5 +4,9 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Eric'}
-    return render_template('index.html', title='Home', user=user)
+    user = {'username': 'Eric'} # NOTE: placeholder
+    posts = [
+        {'author': {'username': 'John'}, 'body': 'God I love Sundays'},
+        {'author': {'username': 'Eric'}, 'body': 'I love getting to do these kinds of things'}
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
