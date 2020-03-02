73075445cfae73a3ba8f1ec270fa0216c7638dfb
Eric Bragas <eric@flywheelsoftware.com>
2020-03-02 01:50:11 +0000

    add user login

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # creates the application object
 # __name__ becomes the name of the package
@@ -9,6 +10,8 @@ app = Flask(__name__)
 app.config.from_object(Config)  # imports config from class; accessible via bracket notation
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
 
 # workaround to circular imports; a flask problem
 from app import routes, models
