6aff3673ea925b85792e856ce17e1240405d01b8
Eric Bragas <eric@flywheelsoftware.com>
2020-03-02 00:21:58 +0000

    add login form and flash functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from config import Config
 
 # creates the application object
 # __name__ becomes the name of the package
 app = Flask(__name__)
+app.config.from_object(Config)  # imports config from class; accessible via bracket notation
 
 # workaround to circular imports; a flask problem
 from app import routes
