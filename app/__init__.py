0b291d9a471dd1901c124efca2ad654227bd6334
Eric Bragas <eric@flywheelsoftware.com>
2020-03-02 00:54:14 +0000

    add and implement migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # creates the application object
 # __name__ becomes the name of the package
 app = Flask(__name__)
 app.config.from_object(Config)  # imports config from class; accessible via bracket notation
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # workaround to circular imports; a flask problem
-from app import routes
+from app import routes, models
