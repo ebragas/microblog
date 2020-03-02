d882c4b7debd512a0614deb1c7272a9e736a91a6
Eric Bragas <eric@flywheelsoftware.com>
2020-03-02 01:54:04 +0000

    fix login route

--- app/routes.py
+++ app/routes.py
@@ -19,7 +19,7 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated():
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
 
     form = LoginForm()
